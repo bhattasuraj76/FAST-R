a6863618bf36e7a487b63d74a615b6e4c2149b91
Suraj Bhatta <bhattasuraj76@gmail.com>
2023-09-14 16:09:10 -0500

    make compatible with python 3.6

--- py/utils.py
+++ py/utils.py
@@ -1,5 +1,6 @@
 # Ported utils helper function from main project
 
+
 def strip_commit_url(repo_url):
     if repo_url:
         return (
@@ -7,23 +8,23 @@ def strip_commit_url(repo_url):
         )
     else:
         return None
- 
+
+
 def get_full_commit_url_by_project(project: str, hash: str) -> str:
-    match project:
-        case "commons-lang":
-            project_url = "https://github.com/apache/commons-lang/commit/"
-        case "joda-time":
-            project_url = "https://github.com/JodaOrg/joda-time/commit/"
-        case "pmd":
-            project_url = "https://github.com/pmd/pmd/commit/"
-        case "gson":
-            project_url = "https://github.com/google/gson/commit/"
-        case "commons-math":
-            project_url = "https://github.com/apache/commons-math/commit/"
-        case "jfreechart":
-            project_url = "https://github.com/jfree/jfreechart/commit/"
-        case "cts":
-            project_url = "https://android.googlesource.com/platform/cts/+/"
+    if project == "commons-lang":
+        project_url = "https://github.com/apache/commons-lang/commit/"
+    elif project == "joda-time":
+        project_url = "https://github.com/JodaOrg/joda-time/commit/"
+    elif project == "pmd":
+        project_url = "https://github.com/pmd/pmd/commit/"
+    elif project == "gson":
+        project_url = "https://github.com/google/gson/commit/"
+    elif project == "commons-math":
+        project_url = "https://github.com/apache/commons-math/commit/"
+    elif project == "jfreechart":
+        project_url = "https://github.com/jfree/jfreechart/commit/"
+    elif project == "cts":
+        project_url = "https://android.googlesource.com/platform/cts/+/"
     return project_url + hash
 
 
@@ -31,8 +32,8 @@ def parse_commit_as_hyperlink_by_project(project: str, hash: str):
     return parse_commit_as_hyperlink(
         get_full_commit_url_by_project(project, hash), hash
     )
-    
+
+
 # Parse commit hash as hyperlink
 def parse_commit_as_hyperlink(url: str, label: str) -> str:
     return f'=HYPERLINK("{url}", "{label}")'
-    
